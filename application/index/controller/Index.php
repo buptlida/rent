45ad5f0e01c2894ded7bef7c95e11e7f4530fb3d
doitwld <doitwld@163.com>
2018-09-02 16:25:31 +0800

    commit

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -9,12 +9,17 @@ class Index extends Controller
 {
     public function index()
     {
-        $list = Db::table('info')->order('firsteditime desc')->paginate(10);
+        $sql = "1=1";
+        if (isset($_POST['keyword'])){
+           $sql.= " and title like '%" . $_POST["keyword"] . "%'";
+        }
+        $list = Db::table('info')->where($sql)->order('firsteditime desc')->paginate(10);
         // 把分页数据赋值给模板变量list
         $this->assign('list', $list);
         // 渲染模板输出
         if(request()->isAjax()) {
            return view("list");
+           //return $this->display($sql);
         }else{
            return $this->fetch();
         }
