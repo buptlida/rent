6d6f130a41a6b3476d3f206946855d9d1749b4a9
doitwld <doitwld@163.com>
2018-09-09 10:44:15 +0800

    commit

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -10,6 +10,41 @@ class Index extends Controller
     public function index()
     {
         $sql = "1=1";
+        $region = Db::table('region')->where($sql)->select();
+        $this->assign('region', $region); 
+        $sql = "1=1";
+        $subline = Db::table('subline')->where($sql)->select();
+        $this->assign('subline', $subline);
+
+        $sql = "1=1";
+        if (isset($_POST['subline'])){
+           if (isset($_POST['subway'])){
+              $sql = "1=1";
+           }else{
+              $sql.= " and title like '%" . $_POST["title"] . "%'";
+              if($_POST['area']!='0') $sql.= " and regioncode =" . $_POST['area'];
+              if($_POST['subline']!='0') $sql.= " and sublinecode =" . $_POST['subline'];
+              if($_POST['housetype']!='0') $sql.= " and housetype =" . $_POST['housetype'];
+              if ($_POST["price"]!='0'){
+                $price = (1 << (intval($_POST["price"]) - 1));
+                $sql.= " and (price & " . strval($price) . " )!=0";
+              } 
+              if($_POST['renttype']=='1') $sql.= " and renttype =" . $_POST['renttype'];
+              if($_POST['renttype']!='1') $sql.= " and renttype !=1";
+              if($_POST['classify']=='1') $sql.= " and classify =" . $_POST['classify'];
+              if($_POST['classify']!='1') $sql.= " and classify !=1";
+              if($_POST['bedroom']!='0') $sql.= " and bedroom =" . $_POST['bedroom'];
+              if($_POST['gender']!='0') $sql.= " and gender =" . $_POST['gender'];
+              if($_POST['publish']=='1') {
+                $mytime=mktime(0, 0, 0, date('m'), date('d')-1, date('Y'));
+                $sql.= " and firsteditime >" .strval($mytime) ;
+              }
+              if($_POST['publish']=='2') {
+                $mytime=mktime(0, 0, 0, date('m'), date('d')-7, date('Y'));
+                $sql.= " and firsteditime >" .strval($mytime) ;
+              }
+           }
+        }
         if (isset($_POST['keyword'])){
            $sql.= " and title like '%" . $_POST["keyword"] . "%'";
         }
@@ -18,10 +53,32 @@ class Index extends Controller
         $this->assign('list', $list);
         // 渲染模板输出
         if(request()->isAjax()) {
-           return view("list");
+           return view("topic");
            //return $this->display($sql);
         }else{
            return $this->fetch();
         }
+    }
+
+    public function getarea()
+    {
+        $sql = "1=1";
+        $code = $_POST['code'];
+        $sql = "1=1 and pcode=".$code;
+        $subregion = Db::table('street')->where($sql)->select();
+        $this->assign('subregion', $subregion);
+        return View("subarea");
+        
+    }
+    
+    public function getstation()
+    {
+        $sql = "1=1";
+        $code = $_POST['code'];
+        $sql = "1=1 and pcode=".$code;
+        $subway = Db::table('subway')->where($sql)->select();
+        $this->assign('subway', $subway);
+        return View("subway");
+        
     }
 }
